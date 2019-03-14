d990d7c68850cb6877fb8b098754215f36b616e2
villa <villasante.carlos@outlook.com>
2019-03-14 23:22:51 +0100

    prueba1

--- PruebaListas.py
+++ PruebaListas.py
@@ -3,13 +3,17 @@ import re
 from stop_words import get_stop_words
 
 
-def function(array, language):
-
-    acceptable_languages = ['es','fr','gr', 'arabic', 'bulgarian', 'catalan', 'czech', 'danish', 'dutch', 'english','finnish', 'french', 'german']
-    if not isinstance(array,str) or language not in acceptable_languages:
+def function(array):
+    acceptable_languages = ['arabic', 'bulgarian', 'catalan', 'czech', 'danish', 'dutch', 'english', 'finnish',
+                            'french', 'german', 'hungarian', 'indonesian', 'italian',
+                            'norwegian', 'polish', 'portuguese', 'romanian', 'russian', 'spanish', 'swedish', 'turkish',
+                            'ukrainian']
+    if not isinstance(array,str):
         raise TypeError
 
-    forbidden_words = get_stop_words(language)
+    forbidden_words = []
+    for language in acceptable_languages:
+        forbidden_words.append(get_stop_words(language))
     list_aux = re.sub("[^\w]", " ", array.lower()).split()
     string_list_lowercase = []
     final_list = []
@@ -21,8 +25,5 @@ def function(array, language):
         counter = string_list_lowercase.count(current_word)
         while current_word in string_list_lowercase:
             string_list_lowercase.remove(current_word)
-
         final_list.append([current_word,counter])
-    return sorted(final_list ,key= lambda x : x[1],reverse=True)
-
-print(function("Me comi Patatas to-wapas el jves, jves, jVes JVES ME me ME comI,,    a a ante contra de",'es'))
+    return sorted(final_list, key=lambda x: x[1],reverse=True)
